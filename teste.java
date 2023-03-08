f906dfd7fc68b3e9b56979e1d1f31a58e41614af
Gabriel <61718293+Gabitil@users.noreply.github.com>
2023-03-08 20:16:52 -0300

    Create teste.java

---
+++ teste.java
@@ -0,0 +1,16 @@
+//faca um programa que leia um numero inteiro e diga se ele e par ou impar
+import java.util.Scanner; //importa a classe Scanner do pacote java.util para o programa poder ler dados do teclado 
+public class teste { //classe principal do programa 
+    public static void main(String[] args) { //metodo principal do programa 
+        Scanner input = new Scanner(System.in); //cria um objeto da classe Scanner para ler dados do teclado 
+        int num; //declara uma variavel do tipo inteiro
+        System.out.println("Digite um numero inteiro: "); //imprime na tela a mensagem entre aspas
+        num = input.nextInt(); //le um numero inteiro do teclado e armazena na variavel num 
+        if (num % 2 == 0) {
+            System.out.println("O numero " + num + " e par"); //imprime na tela a mensagem entre aspas
+        } else {
+            System.out.println("O numero " + num + " e impar");
+        }
+    }
+}
+
